07b4564367b36a48feb1f821dc3837220c08e668
Serhii Khoma <srghma@gmail.com>
2020-08-24 16:31:29 +0300

    feat: button

--- src/HalogenMWC/Button/Common.purs
+++ src/HalogenMWC/Button/Common.purs
@@ -1,13 +1,30 @@
 module HalogenMWC.Button.Common where
 
-import Material.Classes.Button (mdc_button, mdc_button____outlined, mdc_button____raised, mdc_button____touch, mdc_button____unelevated, mdc_button__icon, mdc_button__label, mdc_button__ripple, mdc_button__touch, mdc_touch_target_wrapper)
-import Protolude (Maybe(..))
-
+import DOM.HTML.Indexed (HTMLbutton) as I
 import Data.Array as Array
 import Halogen (AttrName(..), ClassName)
+import Halogen.HTML (IProp)
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
+import Material.Classes.Button (mdc_button, mdc_button____outlined, mdc_button____raised, mdc_button____touch, mdc_button____unelevated, mdc_button__icon, mdc_button__label, mdc_button__ripple, mdc_button__touch, mdc_touch_target_wrapper)
 import MaterialIconsFont.Classes (material_icons)
+import Protolude (Maybe(..))
+
+defaultConfig
+  :: forall props
+   . { disabled :: Boolean
+     , variant :: Variant
+     , touch :: Boolean
+     , additionalAttributes :: Array props
+     }
+defaultConfig =
+  { disabled: false
+  , variant: Text
+  , touch: true
+  , additionalAttributes: []
+  }
+
+------------------
 
 data Variant
   = Text
@@ -30,13 +47,11 @@ commonClasses variant touch =
     , if touch then [ mdc_button____touch ] else []
     ]
 
-commonHtml :: forall configRest w i . { icon :: Maybe String, touch :: Boolean, trailingIcon :: Boolean | configRest } -> String -> Array (HH.HTML w i)
-commonHtml config label = Array.concat
+commonHtml :: forall w i . Boolean -> Array (HH.HTML w i) -> Array (HH.HTML w i)
+commonHtml touch content = Array.concat
   [ [ rippleElt ]
-  , if config.trailingIcon then [] else iconElt config.icon
-  , [ labelElt label ]
-  , if config.trailingIcon then iconElt config.icon else []
-  , if config.touch then [ touchElt ] else []
+  , content
+  , if touch then [ touchElt ] else []
   ]
 
 -----------------
@@ -48,21 +63,22 @@ variantCs variant = case variant of
   Unelevated -> [ mdc_button____unelevated ]
   Outlined -> [ mdc_button____outlined ]
 
-iconElt :: forall w i . Maybe String -> Array (HH.HTML w i)
-iconElt Nothing = []
-iconElt (Just iconName) =
-  [ HH.i
-    [ HP.classes [mdc_button__icon, material_icons]
-    , HP.attr (AttrName "aria-hidden") "true"
-    ]
-    [ HH.text iconName ]
-  ]
-
 rippleElt :: forall w i . HH.HTML w i
 rippleElt = HH.div [ HP.class_ mdc_button__ripple ] []
 
 touchElt :: forall w i . HH.HTML w i
 touchElt = HH.div [ HP.class_ mdc_button__touch ] []
 
-labelElt :: forall w i . String -> HH.HTML w i
-labelElt label = HH.span [ HP.class_ mdc_button__label ] [ HH.text label ]
+------------------
+
+buttonLabel :: forall w i . String -> HH.HTML w i
+buttonLabel label = HH.span [ HP.class_ mdc_button__label ] [ HH.text label ]
+
+buttonIcon :: forall w i . String -> HH.HTML w i
+buttonIcon iconName =
+  HH.i
+    [ HP.classes [mdc_button__icon, material_icons]
+    , HP.attr (AttrName "aria-hidden") "true"
+    ]
+    [ HH.text iconName
+    ]
--- src/HalogenMWC/ButtonLink.purs
+++ src/HalogenMWC/ButtonLink.purs
@@ -12,49 +12,25 @@ import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 import HalogenMWC.Button.Common as Common
 import HalogenMWC.Button.Common (Variant(..))
-import HalogenMWC.Button.Common (Variant(..)) as Export
+import HalogenMWC.Button.Common (Variant(..), buttonLabel, buttonIcon, defaultConfig) as Export
 
-type Config i
-  = { icon :: Maybe String
-    , trailingIcon :: Boolean
-    , disabled :: Boolean
-    , additionalAttributes :: Array (IProp I.HTMLa i) -- put `HE.onClick (\_ -> Increment)` here
-    , touch :: Boolean
-    }
-
-defaultConfig :: forall i . Config i
-defaultConfig =
-  { icon: Nothing
-  , trailingIcon: false
-  , disabled: false -- TODO: link cannot be disabled?
-  , additionalAttributes: []
-  , touch: true
+type Config i =
+  { disabled :: Boolean
+  , variant :: Variant
+  , touch :: Boolean
+  , additionalAttributes :: Array (IProp I.HTMLa i)
   }
 
-button :: forall i w . Variant -> Config i -> String -> HH.HTML w i
-button variant config =
+buttonLink :: forall i w . Config i -> Array (HH.HTML w i) -> HH.HTML w i
+buttonLink config =
   let
     wrapTouch = Common.wrapTouch config.touch
-    commonHtml = Common.commonHtml config
-    commonProps = [ HP.classes (Common.commonClasses variant config.touch) ]
-  in \label ->
+    commonHtml = Common.commonHtml config.touch
+    commonProps = [ HP.classes (Common.commonClasses config.variant config.touch) ]
+  in \content ->
       wrapTouch
         $ HH.element (ElemName "mdc-button")
             [ HP.prop (PropName "disabled") config.disabled
             ]
-            [ HH.a (commonProps <> config.additionalAttributes) (commonHtml label)
+            [ HH.a (commonProps <> config.additionalAttributes) (commonHtml content)
             ]
-
------------------
-
-text :: forall i w . Config i -> String -> HH.HTML w i
-text config label = button Text config label
-
-outlined :: forall i w . Config i -> String -> HH.HTML w i
-outlined config label = button Outlined config label
-
-raised :: forall i w . Config i -> String -> HH.HTML w i
-raised config label = button Raised config label
-
-unelevated :: forall i w . Config i -> String -> HH.HTML w i
-unelevated config label = button Unelevated config label
