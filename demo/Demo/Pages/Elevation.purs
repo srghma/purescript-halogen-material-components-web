d35deb80fa0c19d2b96766bf13d49ca4c6e174c0
Serhii Khoma <srghma@gmail.com>
2020-09-01 20:03:39 +0300

    feat: Elevation

--- demo/Demo/Pages/Elevation.purs
+++ demo/Demo/Pages/Elevation.purs
@@ -1,17 +1,19 @@
 module Demo.Pages.Elevation where
 
 import Demo.HOC.CatalogPage
+import Demo.Utils
+import Halogen
 import Protolude
+
 import Data.Array as Array
 import Data.Maybe as Maybe
-import Halogen
 import Halogen as H
 import Halogen.HTML (IProp)
 import Halogen.HTML as HH
-import Halogen.HTML.Properties as HP
 import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
 import Halogen.HTML.Properties.ARIA as Halogen.HTML.Properties.ARIA
-import Demo.Utils
+import Material.Classes.Elevation
 
 catalogPage :: CatalogPage
 catalogPage =
@@ -22,56 +24,45 @@ catalogPage =
         , documentation: Just "https://package.elm-lang.org/packages/aforemny/material-components-web-elm/latest/Material-Elevation"
         , sourceCode: Just "https://github.com/material-components/material-components-web/tree/master/packages/mdc-elevation"
         }
-    , hero: mkComponentStatic $ HH.div_ heroElevation
+    , hero: mkComponentStatic $ HH.div_
+      [ HH.div [ HP.class_ mdc_elevation____z0, heroSurface ] [ HH.text "Flat 0dp" ]
+      , HH.div [ HP.class_ mdc_elevation____z8, heroSurface ] [ HH.text "Raised 8dp" ]
+      , HH.div [ HP.class_ mdc_elevation____z16, heroSurface ] [ HH.text "Raised 16dp" ]
+      ]
     , content: mkComponentStatic $
-        HH.div demoContainer
-          [ HH.div ([ HP.class_ [ mdc_elevation____z0 ] ] <> demoSurface) [ HH.text "0dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z1 ] ] <> demoSurface) [ HH.text "1dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z2 ] ] <> demoSurface) [ HH.text "2dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z3 ] ] <> demoSurface) [ HH.text "3dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z4 ] ] <> demoSurface) [ HH.text "4dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z5 ] ] <> demoSurface) [ HH.text "5dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z6 ] ] <> demoSurface) [ HH.text "6dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z7 ] ] <> demoSurface) [ HH.text "7dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z8 ] ] <> demoSurface) [ HH.text "8dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z9 ] ] <> demoSurface) [ HH.text "9dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z10 ] ] <> demoSurface) [ HH.text "10dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z11 ] ] <> demoSurface) [ HH.text "11dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z12 ] ] <> demoSurface) [ HH.text "12dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z13 ] ] <> demoSurface) [ HH.text "13dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z14 ] ] <> demoSurface) [ HH.text "14dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z15 ] ] <> demoSurface) [ HH.text "15dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z16 ] ] <> demoSurface) [ HH.text "16dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z17 ] ] <> demoSurface) [ HH.text "17dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z18 ] ] <> demoSurface) [ HH.text "18dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z19 ] ] <> demoSurface) [ HH.text "19dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z20 ] ] <> demoSurface) [ HH.text "20dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z21 ] ] <> demoSurface) [ HH.text "21dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z22 ] ] <> demoSurface) [ HH.text "22dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z23 ] ] <> demoSurface) [ HH.text "23dp" ]
-          , HH.div ([ HP.class_ [ mdc_elevation____z24 ] ] <> demoSurface) [ HH.text "24dp" ]
+        HH.div
+          [ HP.style "display: flex; flex-flow: row wrap; justify-content: space-between;"
+          ]
+          [ HH.div [ HP.class_ mdc_elevation____z0, demoSurface ] [ HH.text "0dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z1, demoSurface ] [ HH.text "1dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z2, demoSurface ] [ HH.text "2dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z3, demoSurface ] [ HH.text "3dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z4, demoSurface ] [ HH.text "4dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z5, demoSurface ] [ HH.text "5dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z6, demoSurface ] [ HH.text "6dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z7, demoSurface ] [ HH.text "7dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z8, demoSurface ] [ HH.text "8dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z9, demoSurface ] [ HH.text "9dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z10, demoSurface ] [ HH.text "10dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z11, demoSurface ] [ HH.text "11dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z12, demoSurface ] [ HH.text "12dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z13, demoSurface ] [ HH.text "13dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z14, demoSurface ] [ HH.text "14dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z15, demoSurface ] [ HH.text "15dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z16, demoSurface ] [ HH.text "16dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z17, demoSurface ] [ HH.text "17dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z18, demoSurface ] [ HH.text "18dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z19, demoSurface ] [ HH.text "19dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z20, demoSurface ] [ HH.text "20dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z21, demoSurface ] [ HH.text "21dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z22, demoSurface ] [ HH.text "22dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z23, demoSurface ] [ HH.text "23dp" ]
+          , HH.div [ HP.class_ mdc_elevation____z24, demoSurface ] [ HH.text "24dp" ]
           ]
     }
 
-heroElevation :: forall r w i . Array (HH.HTML w i)
-heroElevation =
-    [ HH.div ([ HP.class_ [ mdc_elevation____z0 ] ] <> heroSurface) [ HH.text "Flat 0dp" ]
-    , HH.div ([ HP.class_ [ mdc_elevation____z8 ] ] <> heroSurface) [ HH.text "Raised 8dp" ]
-    , HH.div ([ HP.class_ [ mdc_elevation____z16 ] ] <> heroSurface) [ HH.text "Raised 16dp" ]
-    ]
+heroSurface :: forall r i . IProp (style :: String | r) i
+heroSurface = HP.style "display: -ms-inline-flexbox; display: inline-flex; -ms-flex-pack: distribute; justify-content: space-around; min-height: 100px; min-width: 200px; -ms-flex-align: center; align-items: center; width: 120px; height: 48px; margin: 24px; background-color: #212121; color: #f0f0f0;"
 
-heroSurface :: forall r w i . Array (IProp r i)
-heroSurface =
-    [ HP.style "display: -ms-inline-flexbox; display: inline-flex; -ms-flex-pack: distribute; justify-content: space-around; min-height: 100px; min-width: 200px; -ms-flex-align: center; align-items: center; width: 120px; height: 48px; margin: 24px; background-color: #212121; color: #f0f0f0;"
-    ]
-
-demoContainer :: forall r w i . Array (IProp r i)
-demoContainer =
-    [ HP.class_ elevation_demo_container
-    , HP.style "display: flex; flex-flow: row wrap; justify-content: space-between;"
-    ]
-
-demoSurface :: forall r w i . Array (IProp r i)
-demoSurface =
-    [ HP.style "display: -ms-inline-flexbox; display: inline-flex; -ms-flex-pack: distribute; justify-content: space-around; min-height: 100px; min-width: 200px; margin: 15px; -ms-flex-align: center; align-items: center;"
-    ]
+demoSurface :: forall r i . IProp (style :: String | r) i
+demoSurface = HP.style "display: -ms-inline-flexbox; display: inline-flex; -ms-flex-pack: distribute; justify-content: space-around; min-height: 100px; min-width: 200px; margin: 15px; -ms-flex-align: center; align-items: center;"
