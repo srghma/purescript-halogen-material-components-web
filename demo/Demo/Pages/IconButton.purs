5e90cd09e2e269ed261fcc51320d8673266af444
Serhii Khoma <srghma@gmail.com>
2020-09-02 12:06:10 +0300

    feat: icon button wip

--- demo/Demo/Pages/IconButton.purs
+++ demo/Demo/Pages/IconButton.purs
@@ -46,58 +46,70 @@ catalogPage =
         , sourceCode: Just "https://github.com/material-components/material-components-web/tree/master/packages/mdc-icon-button"
         }
     , hero:
-        [ IconToggle.iconToggleMaterialIcons
-            (IconToggle.defaultConfig
-                { on = Set.member "icon-button-hero" state.ons
-                , onChange = Toggle "icon-button-hero"
-                }
-            )
-            { offIcon: "favorite_border"
-            , onIcon: "favorite"
+          H.mkComponent
+            { initialState: const initialState
+            , render: \state ->
+                HH.div_
+                [ IconToggle.iconToggleMaterialIcons
+                    (IconToggle.defaultConfig
+                        { on = Set.member "icon-button-hero" state.ons
+                        , onChange = Just $ const $ Toggle "icon-button-hero"
+                        }
+                    )
+                    { offIcon: "favorite_border"
+                    , onIcon: "favorite"
+                    }
+                ]
+            , eval: H.mkEval H.defaultEval { handleAction = handleAction }
             }
-        ]
     , content:
-        [ HH.h3 [ HP.class_ mdc_typography____subtitle1 ] [ HH.text "Icon Button" ]
-        , IconButton.iconButtonMaterialIcons IconButton.defaultConfig "wifi"
-        , HH.h3 [ HP.class_ mdc_typography____subtitle1 ] [ HH.text "Icon Toggle" ]
-        , IconToggle.iconToggleMaterialIcons
-            (IconToggle.defaultConfig
-                { on = Set.member "icon-button-toggle" state.ons
-                , onChange = Toggle "icon-button-toggle"
-                }
-            )
-            { offIcon: "favorite_border"
-            , onIcon: "favorite"
-            }
-        , HH.h3 [ HP.class_ mdc_typography____subtitle1 ] [ HH.text "Focus Icon Button" ]
-        , HH.div_
-            [ IconButton.iconButtonMaterialIcons
-                (IconButton.defaultConfig
-                    { additionalAttributes = [ HP.id_ "my-icon-button" ]
+        H.mkComponent
+          { initialState: const initialState
+          , render: \state ->
+              HH.div_
+                [ HH.h3 [ HP.class_ mdc_typography____subtitle1 ] [ HH.text "Icon Button" ]
+                , IconButton.iconButtonMaterialIcons IconButton.defaultConfig "wifi"
+                , HH.h3 [ HP.class_ mdc_typography____subtitle1 ] [ HH.text "Icon Toggle" ]
+                , IconToggle.iconToggleMaterialIcons
+                    (IconToggle.defaultConfig
+                        { on = Set.member "icon-button-toggle" state.ons
+                        , onChange = Just $ const $ Toggle "icon-button-toggle"
+                        }
+                    )
+                    { offIcon: "favorite_border"
+                    , onIcon: "favorite"
                     }
-                )
-                "wifi"
-            , HH.text "\x00A0"
-            , Button.button Button.Raised
-                (Button.defaultConfig { additionalAttributes = [ HE.onClick (const $ Focus "my-icon-button") ] })
-                [ HH.text "Focus" ]
-            ]
-        , HH.h3 [ HP.class_ mdc_typography____subtitle1 ] [ HH.text "Focus Icon Toggle" ]
-        , HH.div_
-            [ IconToggle.iconToggleMaterialIcons
-                (IconToggle.defaultConfig
-                    { on = Set.member "icon-button-toggle" state.ons
-                    , onChange = Toggle "icon-button-toggle"
-                    , additionalAttributes = [ HP.id_ "my-icon-toggle" ]
-                    }
-                )
-                { offIcon: "favorite_border"
-                , onIcon: "favorite"
-                }
-            , HH.text "\x00A0"
-            , Button.button Button.Raised
-                (Button.defaultConfig { additionalAttributes = [ HE.onClick $ const $ Focus "my-icon-toggle" ] })
-                [ HH.text "Focus" ]
-            ]
-        ]
+                , HH.h3 [ HP.class_ mdc_typography____subtitle1 ] [ HH.text "Focus Icon Button" ]
+                , HH.div_
+                    [ IconButton.iconButtonMaterialIcons
+                        (IconButton.defaultConfig
+                            { additionalAttributes = [ HP.id_ "my-icon-button" ]
+                            }
+                        )
+                        "wifi"
+                    , HH.text "\x00A0"
+                    , Button.button Button.Raised
+                        (Button.defaultConfig { additionalAttributes = [ HE.onClick (const $ Focus "my-icon-button") ] })
+                        [ HH.text "Focus" ]
+                    ]
+                , HH.h3 [ HP.class_ mdc_typography____subtitle1 ] [ HH.text "Focus Icon Toggle" ]
+                , HH.div_
+                    [ IconToggle.iconToggleMaterialIcons
+                        (IconToggle.defaultConfig
+                            { on = Set.member "icon-button-toggle" state.ons
+                            , onChange = Just $ const $ Toggle "icon-button-toggle"
+                            , additionalAttributes = [ HP.id_ "my-icon-toggle" ]
+                            }
+                        )
+                        { offIcon: "favorite_border"
+                        , onIcon: "favorite"
+                        }
+                    , HH.text "\x00A0"
+                    , Button.button Button.Raised
+                        (Button.defaultConfig { additionalAttributes = [ HE.onClick $ const $ Focus "my-icon-toggle" ] })
+                        [ HH.text "Focus" ]
+                    ]
+                ]
+          , eval: H.mkEval H.defaultEval { handleAction = handleAction }
+          }
     }
