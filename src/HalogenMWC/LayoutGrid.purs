cf7d29dc07a1d4f0e138940a9c22f2cce5926839
Serhii Khoma <srghma@gmail.com>
2020-09-07 19:33:01 +0300

    feat: update

--- src/HalogenMWC/LayoutGrid.purs
+++ src/HalogenMWC/LayoutGrid.purs
@@ -6,14 +6,14 @@ import Halogen.HTML.Properties as HP
 
 -- | ```
 -- | import HalogenMWC.LayoutGrid as LayoutGrid
--- | LayoutGrid.div mdc_layout_grid
+-- | LayoutGrid.divClass mdc_layout_grid
 -- | ```
-div :: forall w i. ClassName -> Array (HH.HTML w i) -> HH.HTML w i
-div class_ = HH.div [ HP.class_ class_ ]
+divClass :: forall w i. ClassName -> Array (HH.HTML w i) -> HH.HTML w i
+divClass class_ = HH.div [ HP.class_ class_ ]
 
 -- | ```
 -- | import HalogenMWC.LayoutGrid as LayoutGrid
--- | LayoutGrid.div' [ mdc_layout_grid ]
+-- | LayoutGrid.divClasses [ mdc_layout_grid ]
 -- | ```
-div' :: forall w i. Array ClassName -> Array (HH.HTML w i) -> HH.HTML w i
-div' classes = HH.div [ HP.classes classes ]
+divClasses :: forall w i. Array ClassName -> Array (HH.HTML w i) -> HH.HTML w i
+divClasses classes = HH.div [ HP.classes classes ]
